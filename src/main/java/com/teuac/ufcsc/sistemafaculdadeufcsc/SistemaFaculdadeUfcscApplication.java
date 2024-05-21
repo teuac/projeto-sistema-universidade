db1e85f8efeb92b57bab6d10348b9923c3c5eff3
teuac <cauetcampos@outlook.com>
2024-05-21 17:24:38 -0300

    project created

---
+++ src/main/java/com/teuac/ufcsc/sistemafaculdadeufcsc/SistemaFaculdadeUfcscApplication.java
@@ -0,0 +1,14 @@
+package com.teuac.ufcsc.sistemafaculdadeufcsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SistemaFaculdadeUfcscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaFaculdadeUfcscApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teuac/ufcsc/sistemafaculdadeufcsc/SistemaFaculdadeUfcscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teuac.ufcsc.sistemafaculdadeufcsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaFaculdadeUfcscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
